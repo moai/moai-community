d26cc182100314583d5f9215082b45d6eb2acff0
halfnelson <dpershouse@gmail.com>
2016-03-25 14:22:07 +1000

    more space handling

--- bin/pito.bat
+++ bin/pito.bat
@@ -6,7 +6,7 @@ if "%MOAI_SDK_HOME%"=="" set "MOAI_SDK_HOME=%~dp0%..\sdk\moai"
 
 
 
-if not exist %MOAI_SDK_HOME% (
+if not exist "%MOAI_SDK_HOME%" (
   echo "Could not find moai sdk, please set MOAI_SDK_HOME to the location of your SDK"
   echo "Looked in: %MOAI_SDK_HOME%"
   exit /b 1
@@ -16,7 +16,7 @@ if not exist %MOAI_SDK_HOME% (
 where moai >nul 2>&1
 if ERRORLEVEL 1 (
 
-   if NOT EXIST %~dp0\moai.exe (
+   if NOT EXIST "%~dp0\moai.exe" (
      echo "Could not find a moai binary in %~dp0 or on the path"
      pause "attempting to build one (ctrl+c to cancel)"
      pushd "%~dp0..\"
@@ -43,7 +43,7 @@ set "INVOKE_DIR=%CD%"
 set MOAI_CMD=%1
 
 
-set args=%INVOKE_DIR% %MOAI_SDK_HOME% %MOAI_CMD%
+set args="%INVOKE_DIR%" "%MOAI_SDK_HOME%" "%MOAI_CMD%"
 
 shift
 
